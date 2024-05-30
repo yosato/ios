d2aa34b3ce67c706358f6abc40ebbe8fb09ecb9d
Yo Sato <yosato16@gmail.com>
2024-05-30 17:42:49 +0100

    stuff

---
+++ goodMatches/goodMatches/ResultHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultHistoryView.swift
+//  goodMatches
+//
+//  Created by Yo Sato on 21/05/2024.
+//
+
+import SwiftUI
+
+struct ResultHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultHistoryView()
+}
---
+++ goodMatches/goodMatches/ResultModel.swift
@@ -0,0 +1,8 @@
+//
+//  ResultModel.swift
+//  goodMatches
+//
+//  Created by Yo Sato on 21/05/2024.
+//
+
+import Foundation
